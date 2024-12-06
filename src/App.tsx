517ce8c23032eb7e860f9bfdac897346484c217b
perolation <perolation@gmail.com>
2024-12-06 17:12:20 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navigation from "@/components/Navigation";
 import Index from "./pages/Index";
 import FindSchool from "./pages/FindSchool";
+import FindSchool from "./pages/Contact";
 
 const queryClient = new QueryClient();
 
